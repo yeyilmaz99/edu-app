c083da895dfd53b67474a808a2bf58202597689d
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-12-09 21:10:17 +0300

    edu-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+import { ProgramComponent } from './components/programs/program/program.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:"full", component:HomeComponent},
+  {path:"programs", loadChildren:() => import('./components/programs/programs.module').then(m => m.ProgramsModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,59 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatPaginatorModule, PageEvent, MatPaginator} from '@angular/material/paginator';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { StoreModule } from '@ngrx/store';
+import { appReducer } from './store/app.state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { CustomSerializer } from './router/custom-serializer';
+import { ProgramEffects } from './components/programs/state/program.effects';
+import { ProgramComponent } from './components/programs/program/program.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    FeedbackComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    OffersComponent,
+    ProgramsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    StoreModule.forRoot(appReducer),
+    StoreDevtoolsModule.instrument({logOnly:environment.production}),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({ serializer: CustomSerializer }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]

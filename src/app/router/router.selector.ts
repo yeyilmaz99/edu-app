5af7e519de748ba8d4f07b3f9d3ae6dfbc5e2268
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-12-09 21:11:36 +0300

    edu-app

---
+++ src/app/router/router.selector.ts
@@ -0,0 +1,13 @@
+import { RouterReducerState } from "@ngrx/router-store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { RouterStateUrl } from "./custom-serializer";
+
+export const getRouterState = createFeatureSelector<RouterReducerState<RouterStateUrl>>('router');
+
+export const getCurrentRoute = createSelector(getRouterState, router => {
+    return router.state;
+})
+
+export const getCurrentId = createSelector(getRouterState,router => {
+    return router.state.params['id']
+})

a82e8cf3337e3f048b7daa987b2865d716d5256f
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-12-09 21:11:28 +0300

    edu-app

---
+++ src/app/models/questionForm.model.ts
@@ -0,0 +1,8 @@
+export interface QuestionForm{
+    programId:number;
+    questionImageData: string;
+    questionHeader:string;
+    questionSubText:string;
+    options:string[];
+    answer:string;
+}

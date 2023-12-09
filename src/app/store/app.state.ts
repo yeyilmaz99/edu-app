5058f9a9cf594933abe76c653ec5ac57a68f5b64
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-12-09 21:11:41 +0300

    edu-app

---
+++ src/app/store/app.state.ts
@@ -0,0 +1,12 @@
+import { programReducer } from "../components/programs/state/program.reducer"
+import { PROGRAM_STATE } from "../components/programs/state/program.selector"
+import { ProgramState } from "../components/programs/state/program.state"
+
+
+export interface AppState {
+
+}
+
+export const appReducer = {
+
+}
